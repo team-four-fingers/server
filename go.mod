02b6563dd445cc53fd8cb27056a96e7c840f4471
Sungmin Cho <samcho9968@banksalad.com>
2023-08-31 13:41:34 +0900

    tidy up project

--- go.mod
+++ go.mod
@@ -3,19 +3,24 @@ module server
 go 1.20
 
 require (
+	github.com/labstack/echo/v4 v4.11.1
+	github.com/pkg/errors v0.9.1
+	github.com/samber/lo v1.38.1
+	github.com/sirupsen/logrus v1.9.3
+	github.com/team-four-fingers/kakao v0.0.0-20230831042225-104693c8b209
+)
+
+require (
 	github.com/go-resty/resty/v2 v2.7.0 // indirect
-	github.com/labstack/echo/v4 v4.11.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/samber/lo v1.38.1 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63 // indirect
-	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
 )
--- server/handler/routes/routes_handler.go
+++ server/handler/routes/routes_handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/samber/lo"
 	"net/http"
 	"server/server"
+	"server/server/handler/common"
 )
 
 var _ server.HTTPHandler = (*RoutesHandler)(nil)
@@ -21,20 +22,22 @@ func (h *RoutesHandler) Path() string {
 
 func (h *RoutesHandler) HandleFunc() func(c echo.Context) error {
 	return func(c echo.Context) error {
+		c.Request()
+
 		resp := &RoutesResponse{
 			//"name": "일진빌딩",
 			//{         "x": "126.946362033068",         "y": "37.5404741779088"     }
-			Origin: &Coordinate{
+			Origin: &common.Coordinate{
 				X: 126.946362033068,
 				Y: 37.5404741779088,
 			},
 			// {         "name": "카카오모빌리티", "x": "127.1101250888609",         "y": "37.39407843730005"     }
-			Destination: &Coordinate{
+			Destination: &common.Coordinate{
 				X: 127.1101250888609,
 				Y: 37.39407843730005,
 			},
 			// [{             "name": "파크원타워1",             "x": "126.92716700037366",         "y": "37.5266641708316"         }]
-			Waypoints: []*Coordinate{
+			Waypoints: []*common.Coordinate{
 				{
 					X: 126.92716700037366,
 					Y: 37.5266641708316,
@@ -47,17 +50,17 @@ func (h *RoutesHandler) HandleFunc() func(c echo.Context) error {
 	}
 }
 
-func roadsToCoordinatesInOrder(roads []*Road) []*Coordinate {
+func roadsToCoordinatesInOrder(roads []*Road) []*common.Coordinate {
 	if len(roads) == 0 {
 		return nil
 	}
 
-	coordinatesInOrder := make([]*Coordinate, 0, lo.SumBy(roads, func(road *Road) int {
+	coordinatesInOrder := make([]*common.Coordinate, 0, lo.SumBy(roads, func(road *Road) int {
 		return len(road.Vertexes) / 2
 	}))
 	for _, road := range roads {
 		for i := 0; i < len(road.Vertexes); i += 2 {
-			coordinatesInOrder = append(coordinatesInOrder, &Coordinate{
+			coordinatesInOrder = append(coordinatesInOrder, &common.Coordinate{
 				X: road.Vertexes[i],
 				Y: road.Vertexes[i+1],
 			})
